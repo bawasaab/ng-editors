efc2b80ca5953345ee211efc0df8c661bab60f79
phpfreak6 <php.freak6@gmail.com>
2021-08-20 19:20:42 +0530

    img

--- src/app/ng2ckeditor/ng2ckeditor.component.ts
+++ src/app/ng2ckeditor/ng2ckeditor.component.ts
@@ -7,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ng2ckeditorComponent implements OnInit {
 
-    ckeditorContent: string = '<p>Some html</p>';
+  ckeConfig: any;
+  ckeditorContent: string = '<p>Some html</p>';
     
   constructor() { }
 
   ngOnInit(): void {
+    this.ckeConfig = {
+      allowedContent: false,
+      extraPlugins: 'divarea,easyimage',
+      forcePasteAsPlainText: true,
+      // filebrowserUploadUrl: 'http://localhost:3000/products/upload-images/611ded7eaccdfd2b3c7e524f',
+      cloudServices_tokenUrl:'http://localhost:3000/products/upload-images/611ded7eaccdfd2b3c7e524f'
+    };
   }
 
+    // https://stackoverflow.com/questions/57414754/how-to-integrate-ckeditor-4-easy-image-into-angular-6
+    // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+    // https://stackoverflow.com/questions/43600877/angular2-overwrite-img-command-in-ng2-ckeditor
 }
